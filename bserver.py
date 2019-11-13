e6cad916b3cbd58e31d6e292e76f29d3a34c22ee
Lekha Sharma <47498066+lekhasharma12@users.noreply.github.com>
2019-11-13 10:58:04 +0530

    Add files via upload

---
+++ bserver.py
@@ -0,0 +1,30 @@
+#Back End server 
+
+from socket import *
+
+Host = '172.16.31.76'
+Port = 4001
+# Port = 4002
+socket1 = socket(AF_INET, SOCK_DGRAM)
+socket1.bind((Host,Port))
+connectedClients=[]
+print("Active",str(Port))
+
+while 1:
+	#print(connectedClients)
+	data, address = socket1.recvfrom(2048)
+	data = data.decode()
+	request = data.split("!")
+	if request[0] == "add":
+		new_client = eval(request[1])
+		if new_client not in connectedClients:
+			connectedClients.append(new_client)
+	elif request[0] == "message":
+		print(data)
+		for i in range(len(connectedClients)):
+					if (connectedClients[i] != eval(request[1])):
+						socket1.sendto((request[1]+":"+request[2]).encode(),connectedClients[i])
+	elif request[0] == "SYN-SENT":
+		socket1.sendto("SYN-RECEIVED".encode(), address)
+	else:
+		print("no case matched")
---
+++ client.py
@@ -0,0 +1,53 @@
+#Multi Server Multi Client Chat Application
+#FE Front End Server
+#BE Back End Server
+
+from socket import *
+import threading 
+import sys
+
+server=input('Please enter IP address to connect: ') #IP Address of the FE Server
+# send an empty message only during the beginning of connection
+
+data = ''
+data = data.ljust(16,'^')
+
+Port = 5002 #Port number of the Front End Server
+socket1 = socket(AF_INET, SOCK_DGRAM)
+socket1.sendto(data.encode(), (server, Port))
+print("----------------Chat Begins---------------------")
+def transmit():
+	while 1:
+		data = input("\n")
+		socket1.sendto(data.encode(), (server, Port))
+		if data == "EXIT" or data == "exit":
+			print("\nExiting chat")
+			print("----------------------------------------------")
+			# second.stop()
+			socket1.close()
+			break
+
+def get(): 
+	while 1: 
+		new_data, address = socket1.recvfrom(2048)
+		new_data = new_data.decode()
+		new_data = new_data.split(':')
+		new_data2 = new_data[1]
+		new_data3 = new_data[0].split(',')
+		if new_data2 != '':
+			print("\n"+str(new_data3[1][1:-1]) +":"+ str(new_data2))
+		if new_data2 ==  "EXIT" or new_data2 == "exit":
+			print("\nExiting chat")
+			print("----------------------------------------------")
+			# first.stop()
+			socket1.close()
+			break
+
+first = threading.Thread(target=transmit)
+second = threading.Thread(target=get)
+
+first.start()
+second.start()
+first.join()
+second.join()
+sys.exit()
